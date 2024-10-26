1516fbd977a50fd3281a2e9e097fb3a0c729dfac
grissharrisdennis <21f3001987@ds.study.iitm.ac.in>
2024-10-26 23:32:37 +0530

    9

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -116,6 +116,6 @@ async function getAccessToken() {
   
   
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${process.env.PORT}`);
+// });
